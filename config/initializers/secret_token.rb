a44bbe80fe3ba4efddb9c68d2a52c31b1854bda4
Thomas St.Onge <stongethomas@gmail.com>
2012-04-26 16:20:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FurEel::Application.config.secret_token = '208832873798bbe5a11b7dac93d90f43c20bd993e8de8decada673ffa5338e46473942d14b16c80f6427f691499403d5544f6c3aee9f131915e55c7bcba287e9'
